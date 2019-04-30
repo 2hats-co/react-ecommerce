7ae6dab9b7a4c48f8c5c80ddfbe3129730f233d4
Sidney Alcantara <sidney@sidney.me>
2019-04-30 14:18:27 +1000

    add theme + some assets

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { pink, amber } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: pink,
+    secondary: amber,
+  },
+
+  typography: { fontFamily: ['system-ui', 'sans-serif'].join(',') },
+
+  shape: { borderRadius: 10 },
+
+  overrides: {
+    MuiButton: {
+      label: {
+        '& > svg': { marginRight: 8 },
+      },
+    },
+  },
+});
+
+export default theme;
